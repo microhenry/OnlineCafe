d64395bb4504e2e7fb27b8e73273383721d40dc7
MicroHenry <microhenry@hotmail.com>
2023-08-21 00:17:52 +1200

    refactor: Beautify login page. Enhanced form validation.

--- cafe-vue/src/views/login.vue
+++ cafe-vue/src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
   <body id="login-page">
-  <el-form class="login-container" label-position="left" label-width="0px" :rules="rules">
+  <el-form class="login-container" label-position="left" label-width="0px" :model="loginForm" :rules="rules" ref="loginForm">
     <h3 class="login_title">Login</h3>
     <el-form-item prop="loginName">
       <el-input
@@ -8,6 +8,7 @@
         v-model="loginForm.loginName"
         auto-complete="off"
         placeholder="login name"
+        clearable
       ></el-input>
     </el-form-item>
     <el-form-item prop="password">
@@ -16,13 +17,14 @@
         v-model="loginForm.password"
         auto-complete="off"
         placeholder="password"
+        show-password
       ></el-input>
     </el-form-item>
     <el-form-item style="width: 100%">
       <el-button
         type="primary"
         style="width: 100%;  border: none"
-        @click="login"
+        @click="login('loginForm')"
       >login</el-button
       >
     </el-form-item>
@@ -53,26 +55,40 @@ export default {
     };
   },
   methods: {
-    login () {
-      var _this = this;
-      userLogin({
-        loginName: this.loginForm.loginName,
-        password: this.loginForm.password,
-      }).then((resp) => {
-        let code=resp.data.code;
-        console.log(code)
-        if(code===200){
-          let data=resp.data.data;
-          let token=data.token;
-          let user=data.user;
-          //存储token
-          _this.$store.commit('SET_TOKENN', token);
-          //存储user，优雅一点的做法是token和user分开获取
-          _this.$store.commit('SET_USER', user);
-          console.log(_this.$store.state.token);
-          var path = this.$route.query.redirect
-          this.$router.replace({path: path === '/' || path === undefined ? '/' : path})
-          console.log('success')
+    login (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          var _this = this;
+          userLogin({
+            loginName: this.loginForm.loginName,
+            password: this.loginForm.password,
+          }).then((resp) => {
+            let code=resp.data.code;
+            console.log(code)
+            if(code===200){
+              let data=resp.data.data;
+              let token=data.token;
+              let user=data.user;
+              //存储token
+              _this.$store.commit('SET_TOKENN', token);
+              //存储user，优雅一点的做法是token和user分开获取
+              _this.$store.commit('SET_USER', user);
+              console.log(_this.$store.state.token);
+              var path = this.$route.query.redirect
+              this.$router.replace({path: path === '/' || path === undefined ? '/' : path})
+              console.log('success')
+            } else {
+              this.$message({
+                showClose: true,
+                message: 'Incorrect username or password.',
+                center: true,
+                type: 'error'
+              });
+            }
+          });
+        } else {
+          console.log('error submit!!');
+          return false;
         }
       });
     }
@@ -82,13 +98,16 @@ export default {
 
 <style scoped>
 #login-page {
+  background: url("../assets/img/bg_login.jpg") no-repeat center;
   height: 100%;
   width: 100%;
   background-size: cover;
   position: fixed;
+  top: 0;
+  left: 0;
 }
 body {
-  margin: 0px;
+  margin: 0;
 }
 .login-container {
   border-radius: 15px;
@@ -102,7 +121,7 @@ body {
 }
 
 .login_title {
-  margin: 0px auto 40px auto;
+  margin: 0 auto 40px auto;
   text-align: center;
   color: #505458;
 }
