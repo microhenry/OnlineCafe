d3dae9c08e512aceb4cb65858d528da2c135be3a
1425T <1390275281@qq.com>
2023-10-15 12:58:07 +1300

    添加money字段

--- cafe-doc/user.sql
+++ cafe-doc/user.sql
@@ -1,34 +1,60 @@
- HEAD
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db01
+-- ------------------------------------------------------
+-- Server version	8.0.34
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-user
-use demo;
--- ----------------------------
--- Table structure for user
--- ----------------------------
+--
+-- Table structure for table `user`
+--
+
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'loginName',
-  `user_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'userName',
-  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'password',
-  `sex` varchar(8) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'gender',
-  `email` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'email',
-  `address` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'address',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `Username` varchar(20) NOT NULL,
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Password` varchar(20) NOT NULL,
+  `Gender` char(1) DEFAULT NULL,
+  `Birthday` date DEFAULT NULL,
+  `Tel` varchar(15) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `Nickname` varchar(20) DEFAULT NULL,
+  `Money` double NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UserAccount_ID_uindex` (`ID`),
+  UNIQUE KEY `UserAccount_Username_uindex` (`Username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `user` VALUES (1, 'zhangsan', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (2, 'zhangsan1', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (3, 'zhangsan2', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (4, 'zhangsan3', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (5, 'zhangsan4', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (6, 'zhangsan5', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (7, 'zhangsan6', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (8, 'zhangsan7', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (9, 'zhangsan8', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (10, 'zhangsan9', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
-INSERT INTO `user` VALUES (11, 'zhangsan10', '张三', '123', '男', '888@qq.com', '呼伦贝尔');
+--
+-- Dumping data for table `user`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
- 0fbc4b5fb526c2aa5fc78c727cdc178d30559557
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('M1',1,'M1','M','2023-09-07','1234567890','Tianjin City',NULL,0),('C1',3,'C1','M','2023-09-07','1357924680','GuangRong Road',NULL,0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-15 12:52:48
