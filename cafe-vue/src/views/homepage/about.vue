e9e79e37508890a19126c3aa88d8533f10a601ee
MicroHenry <microhenry@hotmail.com>
2023-10-25 22:42:33 +1300

    style: Add template content to the about page.

--- cafe-vue/src/views/homepage/about.vue
+++ cafe-vue/src/views/homepage/about.vue
@@ -3,9 +3,24 @@
 </script>
 
 <template>
-  <p>To be updated...</p>
+  <div>
+    <el-card class="about-card">
+      <h2>About Us</h2>
+      <el-row>
+        <el-col :span="12">
+          <p>To be updated...</p>
+        </el-col>
+        <el-col :span="12">
+          <p>To be updated...</p>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <style scoped>
-
+.about-card{
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
