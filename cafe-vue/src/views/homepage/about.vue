79770c0ebccde83b9c1c2bc1df0ea9b425e1d9bc
fanker <205226@stu.hebut.edu.cn>
2023-10-27 00:46:30 +1300

    homepage&about

--- cafe-vue/src/views/homepage/about.vue
+++ cafe-vue/src/views/homepage/about.vue
@@ -3,15 +3,48 @@
 </script>
 
 <template>
-  <div>
+  <div >
     <el-card class="about-card">
-      <h2>About Us</h2>
+      <div class="About_title">
+      <h2 style=" text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);">Co-Coffee in Auckland</h2>
+      <p style=" text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.8);">
+        We hope to bring the unique and innovative coffee experience of Co-Coffee to our customers in every cup of coffee!
+      </p>
+      </div>
       <el-row>
         <el-col :span="12">
-          <p>To be updated...</p>
+          <div class="image-container">
+            <img class="rotating-image" src="../../assets/img/joinus.jpg" alt="Your Image">
+          </div>
+          <h3 class="biaoti">Our Mission and Values</h3>
+          <p>
+            Inspiring and nurturing the spirit of innovation, per person, per cup, per community
+          </p>
+          <p class="content">
+            With our partners, coffee and customers at our core, we endeavour to live the following values:
+          </p>
+           <p class="content">
+          •Create a warm and belonging culture that gladly accepts and happily welcomes everyone.
+            </p>
+          <p class="content">
+        •Act positively, break stereotypes, and innovate to grow the company and our partners together.
+          </p>
+          <p class="content">
+          •In every moment we connect with each other, we are focused and committed, open and honest, with mutual respect and honour.
+          </p>
+          <p class="content">
+           •In everything we do, we do our best, do our best, and take responsibility.
+          </p>
         </el-col>
-        <el-col :span="12">
-          <p>To be updated...</p>
+        <el-col :span="12" class="limitheight">
+          <div class="right-image-coinater">
+            <img class="right-picture" src="../../assets/img/coffeeBean.jpg" alt="Your Image">
+            <p class="des">The coffee maker is not the only factor in making great coffee. The four elements of coffee making are proportion, grind, water and freshness.</p>
+          </div>
+          <div class="right-image-coinater">
+            <img class="right-picture" src="../../assets/img/shouchong.jpg" alt="Your Image">
+            <p class="des">With this simple and easy-to-use method, you can get a cup of coffee that is rich and full-bodied, with a long aftertaste.</p>
+          </div>
         </el-col>
       </el-row>
     </el-card>
@@ -22,5 +55,81 @@
 .about-card{
   width: 80%;
   margin: 0 auto;
+  background-color: #EDEBE9;
 }
+
+.About_title{
+  background-image: url("../../assets/img/background.jpg");
+  background-size: cover; /* 控制背景图片的大小，cover 会填充整个 div */
+  background-position: center; /* 控制背景图片的位置，这里将其居中 */
+  color: white;
+  text-align: center;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.biaoti{
+  text-shadow:  1px 1px 1px #fff,
+  -1px -1px 1px #000;
+  color: #000;
+  font-size: 35px;
+}
+.image-container{
+  width: 200px; /* 设置容器宽度 */
+  height: 200px; /* 设置容器高度，以创建一个圆形容器 */
+  border-radius: 50%; /* 将容器变为圆形 */
+  overflow: hidden; /* 隐藏图片溢出的部分 */
+  transition: transform 0.5s;
+  margin-top: 10px;
+  margin-left: 30%;
+}
+.rotating-image {
+  width: 100%; /* 图片宽度占满容器 */
+  height: 100%; /* 图片高度占满容器 */
+}
+
+.image-container:hover {
+  transform: rotate(90deg); /* 鼠标悬停时旋转容器 */
+}
+.content{
+  text-align: left;
+  margin-top: -1px;
+}
+/**
+* 这是设置右边的元素
+*/
+.limitheight{
+  height: 700px;
+}
+.right-image-coinater{
+  margin-top: 3%;
+  margin-left: 10%;
+  height:45%; /* 设置图片容器的高度为 el-col 的 33% */
+  width: 80%;
+  background: #ffffff;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 5px 5px 5px 2px hsl(0, 0%, 50%);
+}
+.des{
+  padding: 1%;
+}
+
+.right-picture{
+
+  width: 100%; /* 图片宽度占满容器 */
+  height: 70%; /* 图片高度占满容器 */
+  object-fit: cover; /* 保持图片的纵横比并填充容器 */
+  transition: transform 1s; /* 添加变换效果 */
+  /* 默认缩放为正常大小 */
+  transform: scale(1);
+}
+.right-picture:hover {
+  /* 鼠标悬停时放大 1.1 倍 */
+  transform: scale(1.2); /* 可根据需要调整放大的程度 */
+  transform-origin: center center;
+}
+
 </style>
--- cafe-vue/src/views/homepage/home.vue
+++ cafe-vue/src/views/homepage/home.vue
@@ -6,8 +6,8 @@
   <el-col>
     <el-row>
       <el-carousel :interval="4000" type="card" height="360px">
-        <el-carousel-item v-for="item in 6" :key="item">
-          <h3 class="medium">{{ item }}</h3>
+        <el-carousel-item v-for="item in 6" :key="item" class="carousel-item">
+          <img :src="require(`@/assets/img/banners/${item}.jpg`)">
         </el-carousel-item>
       </el-carousel>
     </el-row>
@@ -15,13 +15,17 @@
     <el-row class="homeRow">
       <el-col :span="12" class="homeColImg">
         <el-image
-          :src="requireProductImage()"
+          :src="requireProductImage('logo')"
         >
         </el-image>
       </el-col>
       <el-col :span="12">
-        <h2>Title</h2>
-        <p>Content</p>
+        <h2 class="Title">Co-coffee Shop</h2>
+        <p>Welcome to our online Coffee Shop!<br/>
+          We are trying to do the best Online Coffee Shop in Auckland. In our Coffee Shop, you can find all the coffee and other drinks you want.
+          We use the latest grinding technology to create new coffee flavours and cutting-edge packaging styles.
+         <br/> <br/>We guarantee you the most unique online shopping experience and have our own delivery staff to deliver your drinks to you as soon as possible!
+          </p>
       </el-col>
     </el-row>
   </el-col>
@@ -43,7 +47,6 @@
 .el-carousel__item:nth-child(2n+1) {
   background-color: #d3dce6;
 }
-
 .homeRow {
   padding: 20px;
   height: 400px;
@@ -53,6 +56,19 @@
 .homeColImg {
   justify-content: center;
   align-items: center;
+ margin-top: -6%;
+}
+
+.Title{
+  font-family: "Brush Script MT", serif;
+  color: #6F4E37;
+  font-size: 250%;
+
+}
+.carousel-item img {
+  object-fit: cover; /* 使用cover选项以填充整个容器 */
+  width: 100%;
+  height: 100%;
 }
 </style>
 
@@ -62,7 +78,7 @@ export default {
     requireProductImage(picUrl) {
       try {
         // Try to require the image
-        return require(`../../assets/img/banners/${picUrl}.jpg`);
+        return require(`../../assets/img/banners/${picUrl}.png`);
       } catch (e) {
         // If it fails, return the default image
         return require("../../assets/img/default.jpg");
