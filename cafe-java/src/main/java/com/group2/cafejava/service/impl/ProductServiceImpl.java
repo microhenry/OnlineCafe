aec65b1124947512d6aee3c2871c7c31d7d4658f
xuanWang <1751974693@qq.com>
2023-10-29 23:31:29 +1300

    fix: solve the problem of not deleting products in cart and product meanwhile

--- cafe-java/src/main/java/com/group2/cafejava/service/impl/ProductServiceImpl.java
+++ cafe-java/src/main/java/com/group2/cafejava/service/impl/ProductServiceImpl.java
@@ -1,8 +1,14 @@
 package com.group2.cafejava.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.group2.cafejava.dto.QueryDTO;
+import com.group2.cafejava.entity.Cart;
 import com.group2.cafejava.entity.Product;
+import com.group2.cafejava.mapper.CartMapper;
 import com.group2.cafejava.mapper.ProductMapper;
+import com.group2.cafejava.service.CartService;
 import com.group2.cafejava.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +19,12 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductMapper productMapper;
+
+    @Autowired
+    private CartMapper cartMapper;
+
+    @Autowired
+    private CartService cartService;
 
     @Override
     public Integer addProduct(Product product) {
@@ -33,12 +45,44 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Integer deleteProduct(Integer id) {
-        return productMapper.deleteById(id);
+
+        QueryDTO queryProductDTO = new QueryDTO();
+        queryProductDTO.setPageNo(1);
+        queryProductDTO.setPageSize(9999);
+        String productId = String.valueOf(id);
+        queryProductDTO.setKeyword(productId);
+
+        IPage<Cart> cartWithSameProductId = cartService.selectCartWithSameProductId(queryProductDTO);
+        List<Cart> cartSelect = cartWithSameProductId.getRecords();
+
+        for (Cart cart : cartSelect) {
+            cartService.deleteCart(cart.getCartId());
+        }
+
+            return productMapper.deleteById(id);
     }
 
     @Override
     public void batchDelete(List<Integer> ids) {
+
+        for (Integer id : ids){
+        QueryDTO queryProductDTO = new QueryDTO();
+        queryProductDTO.setPageNo(1);
+        queryProductDTO.setPageSize(9999);
+        String productId = String.valueOf(id);
+        queryProductDTO.setKeyword(productId);
+
+        IPage<Cart> cartWithSameProductId = cartService.selectCartWithSameProductId(queryProductDTO);
+        List<Cart> cartSelect = cartWithSameProductId.getRecords();
+
+        for (Cart cart : cartSelect) {
+            cartService.deleteCart(cart.getCartId());
+        }
+        }
+
+
         productMapper.deleteBatchIds(ids);
+
     }
 
 
